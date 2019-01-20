1d9c72d709662bbfb05c8cd1c25464b855d17979
DanielHartl <daniel.hartl@live.com>
2019-01-19 19:19:25 -0800

    Add tests to CI (#22)
    * Add build script
    
    * Update tests
    
    * Update run-tests.sh
    
    * Update run-tests.sh
    
    * Update TimeRange.cs

---
+++ run-tests.sh
@@ -0,0 +1,24 @@
+cd server
+dotnet test --logger trx
+RESUT=$?
+
+docker build -t server .
+docker rm -f server || true
+docker run --name server -e ASPNETCOREENVIRONMENT=development -d -p 80:80 --rm -ti server
+cd ../integration-tests
+dotnet test --logger trx
+RESIT=$?
+
+docker rm -f server || true
+
+if [ $RESUT -ne 0 ]; then
+  echo Unit tests failed
+  exit $RESUT 
+fi
+
+if [ $RESIT -ne 0 ]; then
+  echo Integration tests failed
+  exit $RESIT
+fi
+
+exit 0
